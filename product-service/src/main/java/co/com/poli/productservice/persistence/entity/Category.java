b6cd7069f19cb8193517d62b4c1bcd764a218f0e
Giovanny Vargas <yovaz05@gmail.com>
2022-05-16 08:48:10 -0500

    clase 12-05-2022

---
+++ product-service/src/main/java/co/com/poli/productservice/persistence/entity/Category.java
@@ -0,0 +1,34 @@
+package co.com.poli.productservice.persistence.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@Table(name ="categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false,nullable = false,unique = true)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ product-service/src/main/java/co/com/poli/productservice/persistence/entity/Product.java
@@ -0,0 +1,42 @@
+package co.com.poli.productservice.persistence.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@Table(name ="products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id",updatable = false,nullable = false,unique = true)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "price")
+    private Double price;
+    @Column(name = "stock")
+    private Double stock;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
